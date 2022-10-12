75bcbfcd125b7897603968610f9ceb1aa2ed5242
playlist7 <115172989+playlist7@users.noreply.github.com>
2022-10-12 20:30:39 +0700

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -4,12 +4,6 @@ $(document).ready(function(){
             $(".content__list-member").toggle(1000)
         })
         $(".gif, .gif2").hide()
-        $(".song[data-index='0'], .btn-toggle-play").click(function(){
-            $(".gif").show(500)
-        })
-        $(".song[data-index!='0']").click(function(){
-            $(".gif").hide(500)
-        })
         $(".firework").hide()
         $(".content__btn-end").click(function(){
             $(".firework").toggle()
